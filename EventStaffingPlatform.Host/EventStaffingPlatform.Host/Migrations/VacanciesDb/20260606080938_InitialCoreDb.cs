43a9e97a39540b8afabc80c247d4499a967c92a0
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-06-06 11:48:46 +0300

    add migration for initial db and adding table of vacancies

---
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/Migrations/VacanciesDb/20260606080938_InitialCoreDb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EventStaffingPlatform.Host.Migrations.VacanciesDb
+{
+    /// <inheritdoc />
+    public partial class InitialCoreDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "vacancies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    requirements = table.Column<string>(type: "text", nullable: false),
+                    functions = table.Column<string>(type: "text", nullable: false),
+                    conditions = table.Column<string>(type: "text", nullable: false),
+                    create_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    close_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    expiration_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vacancies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "vacancies");
+        }
+    }
+}
