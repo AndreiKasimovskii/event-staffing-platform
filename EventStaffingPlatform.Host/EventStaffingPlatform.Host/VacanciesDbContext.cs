915d454cb137f8b050c9607dab96cd8a75cc0ce6
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-06-06 20:34:47 +0300

    add Requirements and RequirementTypes to db context

--- EventStaffingPlatform.Host/EventStaffingPlatform.Host/VacanciesDbContext.cs
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/VacanciesDbContext.cs
@@ -6,6 +6,10 @@ public class VacanciesDbContext(DbContextOptions options) : DbContext(options)
 {
 	public DbSet<Vacancy> Vacancies { get; set; }
 
+	public DbSet<Requirement> Requirements { get; set; }
+
+	public DbSet<RequirementType> RequirementTypes { get; set; }
+
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
 		base.OnModelCreating(modelBuilder);
