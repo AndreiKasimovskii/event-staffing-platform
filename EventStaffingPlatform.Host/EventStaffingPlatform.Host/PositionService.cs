9cc1b50dc3cbe7cc2d024372a5b8c9efbc90578c
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-04 15:16:01 +0300

    change create position method: add validation, create PositionEntity with default properties (CreateDate, Status), add try/catch and define returned type (CreatePositionResult)

--- EventStaffingPlatform.Host/EventStaffingPlatform.Host/PositionService.cs
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/PositionService.cs
@@ -4,27 +4,45 @@ namespace EventStaffingPlatform.Host;
 
 public class PositionService(PositionsDbContext positionsDbContext) : IPositionService
 {
-	public async Task CreatePosition(Position position)
+	public async Task<CreatePositionResult> CreatePosition(PositionDto position)
 	{
-		if (string.IsNullOrWhiteSpace(position.Title))
-			throw new Exception("Title can not be empty!");
-		var conditions = JsonSerializer.Deserialize<Condition[]>(position.Conditions);
+		try
+		{
+			if (string.IsNullOrWhiteSpace(position.Title))
+				return new CreatePositionResult(false, "Title can't be empty!");
+			var conditions = JsonSerializer.Deserialize<Condition[]>(position.Conditions);
 
-		var workingDaysCondition = conditions?.FirstOrDefault(c => c.Name == "working days");
-		var workingHoursCondition = conditions?.FirstOrDefault(c => c.Name == "working hours");
-		var rateCondition = conditions?.FirstOrDefault(c => c.Name == "rate");
+			var workingDaysCondition = conditions?.FirstOrDefault(c => c.Name == "working days");
+			var workingHoursCondition = conditions?.FirstOrDefault(c => c.Name == "working hours");
+			var rateCondition = conditions?.FirstOrDefault(c => c.Name == "rate");
 
-		if (workingDaysCondition is null || workingHoursCondition is null || rateCondition is null)
-			throw new Exception("Job prerequisites are not listed");
+			if (workingDaysCondition is null || workingHoursCondition is null || rateCondition is null)
+				return new CreatePositionResult(false, "Job prerequisites are not listed");
 
-		positionsDbContext.Add(position);
-		await positionsDbContext.SaveChangesAsync();
+			Position newPosition = new()
+			{
+				Title = position.Title,
+				Description = position.Description,
+				Requirements = position.Requirements,
+				Conditions = position.Conditions,
+				CreateDate = DateTimeOffset.UtcNow,
+				Status = PositionStatus.Open
+			};
+
+			positionsDbContext.Add(newPosition);
+			await positionsDbContext.SaveChangesAsync();
+			return new CreatePositionResult(true, null);
+		}
+		catch (Exception ex)
+		{
+			return new CreatePositionResult(false, ex.Message);
+		}
 	}
 }
 
 public interface IPositionService
 {
-	Task CreatePosition(Position position);
+	Task<CreatePositionResult> CreatePosition(PositionDto position);
 }
 
 public sealed class Condition
@@ -33,3 +51,5 @@ public sealed class Condition
 
 	public string? Value { get; set; }
 }
+
+public record CreatePositionResult(bool IsSuccess, string? Error);
