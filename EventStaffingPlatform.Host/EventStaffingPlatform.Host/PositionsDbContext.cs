cfa6641e10c6258955a94da0cf1a6dfcec866484
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-04 15:45:45 +0300

    add condition tempates to DbContext

--- EventStaffingPlatform.Host/EventStaffingPlatform.Host/PositionsDbContext.cs
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/PositionsDbContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EventStaffingPlatform.Host.Conditions;
+using EventStaffingPlatform.Host.Positions;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventStaffingPlatform.Host;
 
 public class PositionsDbContext(DbContextOptions options) : DbContext(options)
 {
 	public DbSet<Position> Positions { get; set; }
+
+	public DbSet<ConditionTemplate> PositionsConditionTemplates { get; set; }
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
