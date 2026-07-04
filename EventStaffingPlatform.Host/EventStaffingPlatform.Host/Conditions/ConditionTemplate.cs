9c2eccaaebf318204abe56669fcc08f6430a24d4
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-04 15:41:45 +0300

    define entity for conditions template and implement entity configuration for creating table and columns

---
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/Conditions/ConditionTemplate.cs
@@ -0,0 +1,12 @@
+﻿namespace EventStaffingPlatform.Host.Conditions;
+
+public class ConditionTemplate
+{
+	public int Id { get; set; }
+
+	public required string Name { get; set; }
+
+	public required string Caption { get; set; }
+
+	public required string Template { get; set; }
+}
---
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/Conditions/PositionsConditionTemplatesEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventStaffingPlatform.Host.Conditions;
+
+public class PositionsConditionTemplatesEntityConfiguration : IEntityTypeConfiguration<ConditionTemplate>
+{
+	public void Configure(EntityTypeBuilder<ConditionTemplate> builder)
+	{
+		builder.ToTable("position_condition_templates");
+
+		builder.Property(c => c.Name)
+			.HasMaxLength(64)
+			.HasColumnName("name")
+			.IsRequired();
+
+		builder.Property(c => c.Caption)
+			.HasMaxLength(256)
+			.HasColumnName("caption")
+			.IsRequired();
+
+		builder.Property(c => c.Template)
+			.HasColumnName("template")
+			.HasColumnType("jsonb")
+			.IsRequired();
+
+		builder.HasIndex(c => c.Name).IsUnique();
+	}
+}
