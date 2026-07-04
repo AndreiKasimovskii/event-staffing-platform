a749f5fd1b256cba94badf291eca40c41e2c4205
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-04 15:45:19 +0300

    create migration for condition templates dictionary

---
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/Migrations/20260704123952_CreateConditionsDictionary.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EventStaffingPlatform.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateConditionsDictionary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PositionsConditionTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Caption = table.Column<string>(type: "text", nullable: false),
+                    Template = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PositionsConditionTemplates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PositionsConditionTemplates");
+        }
+    }
+}
