71fd7aa7cd07bd4beb6e414a1b9e1d8401119c14
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-04 15:13:10 +0300

    rename property at DbContext for positions

--- EventStaffingPlatform.Host/EventStaffingPlatform.Host/PositionsDbContext.cs
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/PositionsDbContext.cs
@@ -4,7 +4,7 @@ namespace EventStaffingPlatform.Host;
 
 public class PositionsDbContext(DbContextOptions options) : DbContext(options)
 {
-	public DbSet<Position> Vacancies { get; set; }
+	public DbSet<Position> Positions { get; set; }
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
