59babf8527f7f94fa3a263864c3722f0e7336146
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-09 10:16:41 +0300

    create EntityConfiguration for EventStorageEntity

---
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/Events/EventEntityConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventStaffingPlatform.Host.Events;
+
+public class EventEntityConfiguration : IEntityTypeConfiguration<EventStorageEntity>
+{
+	public void Configure(EntityTypeBuilder<EventStorageEntity> builder)
+	{
+		builder.ToTable("events");
+
+		builder.Property(e => e.Id)
+			.HasColumnName("event_id");
+
+		builder.Property(e => e.Title)
+			.HasColumnName("title")
+			.HasMaxLength(512)
+			.IsRequired();
+
+		builder.Property(e => e.Description)
+			.HasColumnName("description");
+
+		builder.Property(e => e.Address)
+			.HasColumnName("address")
+			.HasMaxLength(128)
+			.IsRequired();
+
+		builder.Property(e => e.StartEventDate)
+			.HasColumnName("start_event_date")
+			.IsRequired();
+
+		builder.Property(e => e.EndEventDate)
+			.HasColumnName("end_event_date")
+			.IsRequired();
+	}
+}
