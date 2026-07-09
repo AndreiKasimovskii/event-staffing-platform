50f310621ce803d2b25b8d3d62f3ca86ef8c4f5d
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-09 10:17:10 +0300

    added event context to database

---
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/Events/EventDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventStaffingPlatform.Host.Events;
+
+public class EventDbContext(DbContextOptions options) : DbContext(options)
+{
+	public DbSet<EventStorageEntity> Events { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.ApplyConfiguration(new EventEntityConfiguration());
+	}
+}
