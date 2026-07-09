e51299b4da1c064a8146c4a7679641f9406a5f98
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-09 10:17:52 +0300

    create first migration: InitialMigrationToEventsDb

---
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/Migrations/20260709070322_InitialMigrationToEventsDb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EventStaffingPlatform.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrationToEventsDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    event_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    address = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    start_event_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    end_event_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_events", x => x.event_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events");
+        }
+    }
+}
