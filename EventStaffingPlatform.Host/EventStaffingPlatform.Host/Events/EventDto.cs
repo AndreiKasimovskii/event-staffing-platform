cb1f395731919f06d5a24fa3f960cdeb7cfdb8fb
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-18 15:50:43 +0300

    create new dto for present events

--- EventStaffingPlatform.Host/EventStaffingPlatform.Host/Events/EventDto.cs
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/Events/EventDto.cs
@@ -1,3 +1,5 @@
 ﻿namespace EventStaffingPlatform.Host.Events;
 
-public record EventDto(string Title, string? Description, string Address, DateTimeOffset StartEventDate, DateTimeOffset EndEventDate);
+public record EventListItemResponse(int Id, string Title, DateTimeOffset StartEventDate, DateTimeOffset EndEventDate);
+
+public record EventDetailsResponse(int Id, string Title, string? Description, string Address, DateTimeOffset StartEventDate, DateTimeOffset EndEventDate);
