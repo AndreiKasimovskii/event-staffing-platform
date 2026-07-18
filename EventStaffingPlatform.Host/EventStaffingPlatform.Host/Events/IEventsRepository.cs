b2d775ddf709d9ceaa0fd6de5f267b0ca13d4458
Andrei Kasimovskii <kasimovskiy.a.d@gmail.com>
2026-07-18 15:54:11 +0300

    added methods for getting events

--- EventStaffingPlatform.Host/EventStaffingPlatform.Host/Events/IEventsRepository.cs
+++ EventStaffingPlatform.Host/EventStaffingPlatform.Host/Events/IEventsRepository.cs
@@ -3,4 +3,27 @@
 public interface IEventsRepository
 {
 	Task<bool> CreateEventAsync(EventStorageEntity entity);
+
+	/// <summary>
+	/// Получение всех мероприятий
+	/// </summary>
+	/// <param name="cancellationToken">Токен отмены выполнения операции</param>
+	/// <returns>Мероприятия</returns>
+	Task<EventStorageEntity[]> GetAllEventsAsync(CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Получение акутальных мероприятий
+	/// </summary>
+	/// <param name="currentDate">Текущая дата</param>
+	/// <param name="cancellationToken">Токен отмены операции</param>
+	/// <returns>Актуальные мероприятия</returns>
+	Task<EventStorageEntity[]> GetActualEventsAsync(DateTimeOffset currentDate, CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Получение мероприятия по идентификатору
+	/// </summary>
+	/// <param name="id">Идентификатор</param>
+	/// <param name="cancellationToken">Токен отмены операции</param>
+	/// <returns>Данные мероприятия</returns>
+	Task<EventStorageEntity?> GetEventByIdAsync(int id, CancellationToken cancellationToken);
 }
